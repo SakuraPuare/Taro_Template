2909ce83da20eaf6893c57e18d607d83a7ff1da8
Steven Moder <java20131114@gmail.com>
2025-03-23 17:52:44 +0800

    在配置文件中添加 injectAdditionalCssVarScope 选项，以支持额外的 CSS 变量作用域注入。

--- config/index.js
+++ config/index.js
@@ -51,6 +51,7 @@ const config = {
             args: [
               {
                 appType: 'taro',
+                injectAdditionalCssVarScope: true,
               },
             ],
           },
